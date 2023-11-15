7e0b283acb916f3bcb557bfddda990820a2d13b9
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-15 16:05:20 +0530

    <feat>: [adds the title and description of all 26 diagnosis]

--- klepio-frontend/src/services/resultDecoder.jsx
+++ klepio-frontend/src/services/resultDecoder.jsx
@@ -47,70 +47,70 @@ const results = [
     description: "Food impaction with acute reversible pulpitis means food trapped between teeth has caused temporary inflammation in the dental pulp. Symptoms include pain and sensitivity to hot or cold liquids or foods specifically in the area of the trapped food debris. Once the food particle is removed and the area is cleaned, the symptoms typically resolve indicating the pulpitis is reversible. Proper flossing prevents food from becoming impacted.",
     image: "",
   },  {
-    title: "",
-    description: ,
-    image: ,
+    title: "Aphthous Ulcer",
+    description: "An aphthous ulcer is a common, painful sore or lesion that forms on the mucous membranes of the mouth including the gums and tongue. The exact cause is unknown but they often form after an injury, periods of stress, lack of sleep, or vitamin/nutrient deficiencies. Aphthous ulcers are not contagious. Symptoms include a tingling or burning sensation followed by a painful sore with a white or yellow center and red rim. They will heal on their own within a week or two. Treatments can provide pain relief and speed healing.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Aphthous Ulcer",
+    description: "An aphthous ulcer is a common, painful sore or lesion that forms on the mucous membranes of the mouth including the gums and tongue. The exact cause is unknown but they often form after an injury, periods of stress, lack of sleep, or vitamin/nutrient deficiencies. Aphthous ulcers are not contagious. Symptoms include a tingling or burning sensation followed by a painful sore with a white or yellow center and red rim. They will heal on their own within a week or two. Treatments can provide pain relief and speed healing.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Chronic Gingivitis",
+    description: "Chronic gingivitis is persistent inflammation of the gums caused by a prolonged buildup of plaque from poor oral hygiene. Symptoms include tender, swollen gums that may recede from the teeth and bleed easily during brushing and flossing. Treatment requires professional dental cleanings and improved home care like brushing and flossing to remove plaque. If not treated, chronic gingivitis can advance to periodontitis and tooth loss.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Chronic Gingivitis with teeth mobility",
+    description: "When chronic gingivitis is accompanied by tooth mobility, it indicates progressive periodontal disease. The inflammation has begun to damage connective tissues like the periodontal ligament that holds the teeth in the socket. Pockets form allowing more plaque buildup and infection continues spreading. Treatment requires deep cleanings, antibiotics and bite splints to reduce mobility. Otherwise tooth loss will occur.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Chronic destructive periodontitis",
+    description: "Chronic destructive periodontitis is advanced gum disease causing progressive damage to the periodontal ligament and jawbone that support the teeth. Symptoms include swollen, tender gums that bleed easily, bad breath, loose or separating teeth, and pus around teeth. It is caused by unchecked plaque buildup. Treatment involves deep cleanings and antibiotics. The damage cannot be reversed so prevention is key through diligent oral hygiene.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Dental abcess",
+    description: "A dental abscess is a severe infection of a tooth, the gums or jawbone characterized by pus buildup, swelling and severe throbbing pain. Abscesses occur when untreated tooth decay or injury allows bacteria to infect the tooth and spread via the root to the bone. Treatment involves draining the pus and antibiotics. The infected tooth will also need a root canal or extraction. Left untreated, a dental abscess can spread causing serious complications.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Dental abcess",
+    description: "A dental abscess is a severe infection of a tooth, the gums or jawbone characterized by pus buildup, swelling and severe throbbing pain. Abscesses occur when untreated tooth decay or injury allows bacteria to infect the tooth and spread via the root to the bone. Treatment involves draining the pus and antibiotics. The infected tooth will also need a root canal or extraction. Left untreated, a dental abscess can spread causing serious complications.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Dental caries with food impaction",
+    description: "Dental caries (tooth decay) that causes a food impaction indicates advanced decay likely reaching the inner pulp layer. Symptoms include pain and sensitivity when eating as food gets trapped between the decayed teeth. The trapped food irritates the already inflamed pulp. Treatment requires removing the trapped food and decayed portions of the tooth via fillings or possibly a root canal if the pulp is infected. Reducing food impactions via proper flossing and treating early decay helps prevent this.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Denture sore ulcer",
+    description: "A denture sore is an ulcer or sore spot that forms on the gums or other oral tissues underneath or due to ill-fitting dentures. Causes include inadequate fit, poor denture hygiene, accumulation of food, and tissue irritation or trauma from the dentures. Symptoms include pain, swelling, redness or bleeding in the sore area. Treatment requires adjusting the dentures for proper fit and keeping them and the mouth very clean. The ulcer heals once friction and irritation from the dentures is minimized.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Extra oral swelling with Acute Irreversible Pulpitis",
+    description: "When acute irreversible pulpitis is accompanied by extra oral swelling, the infection has extended beyond the tooth roots and dental pulp. Significant swelling of the cheek, lips or facial tissues indicates a spreading infection called a dental abscess. Along with the swelling, symptoms include severe, throbbing pain that may radiate along the jaw and to the ear or neck on the affected side. This condition requires prompt dental treatment including potent antibiotics and drainage of the abscess. Root canal treatment or extraction of the infected tooth will also be needed to remove the original source of infection. Left untreated, the infection can continue spreading deeper through tissues or into the bloodstream with dangerous complications. The swelling and pain should resolve once the tooth is treated or removed and antibiotics administered to control the infection.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Food impaction",
+    description: "Food impaction refers to food trapped tightly between two teeth that cannot be removed by brushing or flossing alone. Symptoms include pain, inflammation, or sensitivity between the teeth where the food is lodged. It can lead to secondary problems like tooth decay, infection, or damage to tooth enamel. Treatment requires having the stuck food particles removed by a dentist using special tools. Preventive measures involve proper flossing technique and avoiding sticky, dense foods that can easily become packed between teeth.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Pericoronitis",
+    description: "Pericoronitis refers to inflammation of the gum tissue surrounding the crown portion of a tooth that has not yet fully emerged through the gums. It commonly affects wisdom teeth. Symptoms include pain, swelling, and redness of the gum tissue over the impacted tooth. Treatment involves good oral hygiene, warm salt water rinses, and sometimes antibiotics. Partially erupted teeth may require extraction if gum irritation and infection persists.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Periodontal abcess",
+    description: "A periodontal abscess is an abscess located in the tissues surrounding and supporting the teeth - the periodontium. It is caused by an infection, often advanced gum disease. Symptoms include swelling, redness, bleeding from the gums around a tooth, and severe throbbing pain. Treatment involves antibiotics and drainage of the pus. The infected tooth may also need a deep cleaning or extraction.",
+    image: "",
   },  {
-    title: ,
-    description: ,
-    image: ,
-  },  {
-    title: ,
-    description: ,
-    image: ,
+    title: "Periodontitis",
+    description: "Periodontitis refers to inflammation and infection of the periodontium which includes the gums, periodontal ligament, and jawbone that surround and support the teeth. If left untreated, the infection destroys these supporting tissues leading to tooth loss. Symptoms include swollen, bleeding gums, receding gums, loose teeth, and persistent bad breath. It is caused by a buildup of plaque and tartar due to poor oral hygiene. Treatment involves professional dental cleanings, antibiotics, and diligent oral hygiene.",
+    image: "",
+  },   {
+    title: "Periodontitis",
+    description: "Periodontitis refers to inflammation and infection of the periodontium which includes the gums, periodontal ligament, and jawbone that surround and support the teeth. If left untreated, the infection destroys these supporting tissues leading to tooth loss. Symptoms include swollen, bleeding gums, receding gums, loose teeth, and persistent bad breath. It is caused by a buildup of plaque and tartar due to poor oral hygiene. Treatment involves professional dental cleanings, antibiotics, and diligent oral hygiene.",
+    image: "",
   },
   {
-    title: ,
-    description: ,
-    image: ,
+    title: "Pulpitis with swelling",
+    description: "Pulpitis with swelling indicates the pulp inflammation has extended beyond the tooth into the surrounding periodontal tissues. The swollen gums will be tender to touch and may appear reddish. Pain may be severe and radiating. This condition needs urgent dental care to treat the source of the pulpitis, which may be decay or a cracked tooth. The swelling should subside once the tooth is treated with a filling, crown, or root canal as needed.",
+    image: "",
   },
 ];
 
@@ -119,7 +119,8 @@ const ResultDecoder = () => {
     <div className='w-full flex flex-col justify-center items-center'>
       {results.map((result, key) => (
         <div key={key} className='bg-white text-black'>
-          {result.description}
+            <h1 className='font-bold text-3xl font-pontiac'>{result.title}</h1>
+          <p className='font-poppins text-xl'>{result.description}</p>
           <img src={result.image} alt='image' />
         </div>
       ))}
