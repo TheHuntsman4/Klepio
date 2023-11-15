a1dd2048adcbe4cc8b90cc1a3683e524059eed4b
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-16 01:41:00 +0530

    <fix>: [removes some comments]

--- klepio-frontend/src/services/resultDecoder.jsx
+++ klepio-frontend/src/services/resultDecoder.jsx
@@ -1,12 +1,6 @@
-import results from './resultArray';
+import results from "./resultArray";
 
 export function resultDecoder(index) {
-
-  // Get result at index
   const result = results[index];
-
-  // Return result object
   return result;
-
 }
-  
