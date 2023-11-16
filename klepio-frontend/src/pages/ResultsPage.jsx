e8fc6fe618a96fba0e40a3931e49c95ad18e6049
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-16 12:11:19 +0530

    <feat>: [adds buttons in the resultsPage]

--- klepio-frontend/src/pages/ResultsPage.jsx
+++ klepio-frontend/src/pages/ResultsPage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import DeskTopGreenBG from "../assets/nonicons/DesktopFullGreenBG.png";
 import MobileGreenBG from "../assets/nonicons/MobileFullGreenBG.png";
-import results from "../services/resultArray";
+import { Link } from "react-router-dom";
 import { resultDecoder } from "../services/resultDecoder";
 const ResultsPage = () => {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-device-width: 1224px)",
   });
-  const diagnosis = resultDecoder(5); 
+  const diagnosis = resultDecoder(5);
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-full min-h-screen w-full">
       <img
         src={isDesktopOrLaptop ? DeskTopGreenBG : MobileGreenBG}
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
@@ -28,12 +29,23 @@ const ResultsPage = () => {
             <img src={diagnosis.image} width={500} />
           </div>
         </div>
-        <p className="w-1/2 font-poppins text-xl text-center">
+        <p className=" w-5/6 lg:w-1/2 font-poppins text-xl text-center">
           Although this AI model has astounding accuracy for diagnosis, we
           recommend confirming the diagnosis and seeking further treatment from
           a professional dentist or medical practitioner, Klepio’s results can
           not substitute true diagnosis
         </p>
+        <div className="flex flex-col lg:flex-row justify-center items-center">
+          <button className="bg-black rounded-full my-4 lg:mr-4  px-12 py-4 text-white text-lg">
+            <Link to="/diagnose">Retake Test</Link>
+          </button>
+          <button className="bg-black rounded-full my-4 lg:mr-4 px-12 py-4 text-white text-lg">
+            <Link to="/diagnose">Download Results</Link>
+          </button>
+          <button className="bg-black rounded-full my-4 px-12 py-4 text-white text-lg">
+            <Link to="/diagnose">Affiliated Doctors</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
