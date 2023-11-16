ca126e72d66e3469d5b20a7aa12303a72e799a05
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-16 13:20:26 +0530

    <feat>: [starts development of diagnosis form]

--- klepio-frontend/src/pages/DiagnoseStartPage.jsx
+++ klepio-frontend/src/pages/DiagnoseStartPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import DeskTopGreenBG from "../assets/nonicons/DesktopFullGreenBG.png";
+import MobileGreenBG from "../assets/nonicons/MobileFullGreenBG.png";
 
 const DiagnoseStartPage = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  let answers = {}
   return (
-    <div>
-      
+    <div className="relative h-full min-h-screen w-full">
+      <img
+        src={isDesktopOrLaptop ? DeskTopGreenBG : MobileGreenBG}
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DiagnoseStartPage
+export default DiagnoseStartPage;
