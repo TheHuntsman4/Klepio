ca65ffcc0c3932caa81ef8df2fd805b8f5ae6d77
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-16 20:04:19 +0530

    <feat>: [adds export from the painPages directory]

--- klepio-frontend/src/pages/index.js
+++ klepio-frontend/src/pages/index.js
@@ -2,3 +2,7 @@ export {default as LandingPage} from './LandingPage';
 export {default as HomePage} from './HomePage';
 export {default as DiagnoseStartPage} from './DiagnoseStartPage';
 export {default as ResultsPage} from './ResultsPage'
+export {default as Pain1} from './painPages/pain1';
+export {default as Pain2} from './painPages/pain2';
+export {default as Pain3} from './painPages/pain3';
+export {default as Pain4} from './painPages/pain4';
