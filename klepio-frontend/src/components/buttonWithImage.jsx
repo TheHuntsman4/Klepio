db90cb1282b5f9907e1d0af63a3e9f48ce7953c9
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-16 16:22:43 +0530

    <feat>: [minor UI changes]

--- klepio-frontend/src/components/buttonWithImage.jsx
+++ klepio-frontend/src/components/buttonWithImage.jsx
@@ -9,10 +9,10 @@ const ButtonWithImage = ({ title, image, id }) => {
   return (
     <button
       id={id}
-      className="w-full h-full flex flex-col justify-center items-center text-2xl font-poppins font-semibold bg-white"
+      className="w-full h-full flex flex-col justify-center items-center text-2xl font-poppins font-semibold bg-white rounded-3xl lg:p-12"
     >
-      <img src={image} width={isDesktopOrLaptop ? 150 : 75} alt={title} />
-      <p>{title}</p>
+      <img src={image} width={isDesktopOrLaptop ? 150 : 75} alt={title} className=""/>
+      <p className="mt-12 text-center">{title}</p>
     </button>
   );
 };
--- klepio-frontend/src/pages/DiagnoseStartPage.jsx
+++ klepio-frontend/src/pages/DiagnoseStartPage.jsx
@@ -3,7 +3,7 @@ import { useMediaQuery } from "react-responsive";
 import DeskTopGreenBG from "../assets/nonicons/DesktopFullGreenBG.png";
 import MobileGreenBG from "../assets/nonicons/MobileFullGreenBG.png";
 import { ButtonWithImage } from "../components";
-import pain from "../assets/icons/questionnairepng/electrict.png";
+import pain from "../assets/icons/questionnairepng/achingt.png";
 import swelling from "../assets/icons/questionnairepng/swellingt.png";
 import ulcer from "../assets/icons/questionnairepng/ulcert.png";
 
@@ -29,12 +29,12 @@ const DiagnoseStartPage = () => {
         alt="bg"
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
       />
-      <div className="w-full h-full flex flex-col justify-center items-center">
-        <p className="text-xl font-poppins">
-          Which of the following <b>best describes</b> the chief problem you are
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <p className="text-3xl font-poppins mb-12">
+          1. Which of the following <b>best describes</b> the chief problem you are
           facing?
         </p>
-        <div className="w-1/2 grid grid-cols-2 lg:grid-cols-3 justify-center items-center gap-2 z-20">
+        <div className="w-1/2 grid grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4 z-20">
           {options.map((option, key) => {
             return (
               <ButtonWithImage
