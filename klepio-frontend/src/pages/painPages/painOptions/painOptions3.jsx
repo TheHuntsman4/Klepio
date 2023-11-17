649224057be38bc42eb03666e24448686616e49a
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-17 14:52:31 +0530

    <feat>: [adds all options for pain]

---
+++ klepio-frontend/src/pages/painPages/painOptions/painOptions3.jsx
@@ -0,0 +1,7 @@
+import sudden from '../../../assets/icons/questionnairepng/stabt';
+import linger from '../../../assets/icons/questionnairepng/ript';
+const options3 = [
+  { title: "Pain Came Suddenly", image: sudden, code: "1" },
+  { title: "Lingering Pain", image: linger, code: "2" },
+];
+export default options3
---
+++ klepio-frontend/src/pages/painPages/painOptions/painOptions4.jsx
@@ -0,0 +1,7 @@
+const options4 = [
+  { title: "Cold Foods", image: "", code: "0" },
+  { title: "Eating", image: "", code: "1" },
+  { title: "Hot Foods", image: "", code: "2" },
+  { title: "Laying Down", image: "", code: "3" },
+];
+export default options4;
