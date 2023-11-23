aafedcf23939908b7a7eca6f903443d6caba1720
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-23 22:11:00 +0530

    <feat>: [updates the options for common page]

--- klepio-frontend/src/pages/commonPages/commonOptions/commonOptions2.jsx
+++ klepio-frontend/src/pages/commonPages/commonOptions/commonOptions2.jsx
@@ -1,7 +1,12 @@
+import deg0 from "../../../assets/icons/questionnaire/degree0.svg";
+import deg1 from "../../../assets/icons/questionnaire/degree0.svg";
+import deg2 from "../../../assets/icons/questionnaire/degree1";
+import deg3 from "../../../assets/icons/questionnaire/degree3";
+
 const options2 = [
-  { title: "No", code: "0" },
-  { title: "1", code: "1" },
-  { title: "2", code: "2" },
-  { title: "3", code: "3" },
+  { title: "No", image: deg0, code: "0" },
+  { title: "1", image: deg1, code: "1" },
+  { title: "2", image: deg2, code: "2" },
+  { title: "3", image: deg3, code: "3" },
 ];
 export default options2;
