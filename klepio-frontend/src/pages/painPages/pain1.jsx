3a4a84b62d3ab568074252a7b969a19f58bd6dd8
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-30 00:55:13 +0530

    <feat>: [integrates fetchData function in pain1]

--- klepio-frontend/src/pages/painPages/pain1.jsx
+++ klepio-frontend/src/pages/painPages/pain1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { QuestionPage1, QuestionPage2 } from "../../components";
 import { useMediaQuery } from "react-responsive";
 import DeskTopGreenBG from "../../assets/nonicons/DesktopFullGreenBG.png";
@@ -9,6 +9,7 @@ import options2 from "../../services/painOptions/painOptions2";
 import options3 from "../../services/painOptions/painOptions3";
 import options4 from "../../services/painOptions/painOptions4";
 import { useLocation, useNavigate } from "react-router-dom";
+import fetchData from "../../services/fetchData";
 
 const Pain1 = () => {
   const navigate = useNavigate();
@@ -21,31 +22,39 @@ const Pain1 = () => {
   const [severity, setSeverity] = useState("");
   const [onset, setOnset] = useState("");
   const [worse, setWorse] = useState("");
+  const [prediction, setPrediction] = useState();
 
-  const onContinueEnd = () => {
+  const onContinueEnd = async () => {
     answers = {
       ...answers,
-      "Nature_of_Pain": nature,
-      "Severity_of_pain": severity,
-      "Onset_and_mode_of_pain": onset,
-      "Factors_which_worsens_the_pain": worse,
-      "Is_the_swelling_painful": "0",
-      "Has_the_swelling_changed_since_it_was_first_noticed": "0",
-      "Does_the_swelling_changes_during_normal_activities": "0",
-      "Is_the_ulcer_painful": "0",
-      "Is_there_bleeding_from_the_ulcer": "0",
-      "Is_there_discharge_from_the_ulcer": "0",
-      "Is_there_a_foul_smell_from_the_ulcer": "0",
-      "Do_the_ulcers_interfere_with_daily_activities": "0",
-      "Has_the_ulcer_changed_since_first_noticed": "0",
-      "Have_you_had_similar_ulcers": "0",
-      "Is_there_bleeding_in_the_gums": "0",
-      "Is_there_pain_in_the_gums": "0",
-      "If_any_tooth_teeth_is_are_mobile_what_is_the_degree_of_mobility": "0"
+      Nature_of_Pain: nature,
+      Severity_of_pain: severity,
+      Onset_and_mode_of_pain: onset,
+      Factors_which_worsens_the_pain: worse,
+      Is_the_swelling_painful: "0",
+      Has_the_swelling_changed_since_it_was_first_noticed: "0",
+      Does_the_swelling_changes_during_normal_activities: "0",
+      Is_the_ulcer_painful: "0",
+      Is_there_bleeding_from_the_ulcer: "0",
+      Is_there_discharge_from_the_ulcer: "0",
+      Is_there_a_foul_smell_from_the_ulcer: "0",
+      Do_the_ulcers_interfere_with_daily_activities: "0",
+      Has_the_ulcer_changed_since_first_noticed: "0",
+      Have_you_had_similar_ulcers: "0",
+      Is_there_bleeding_in_the_gums: "0",
+      Is_there_pain_in_the_gums: "0",
+      If_any_tooth_teeth_is_are_mobile_what_is_the_degree_of_mobility: "0",
     };
     console.log(answers);
-    navigate("/diagnose/common", { state: { answers: answers } });
+    const response = await fetchData(answers);
+    setPrediction(response);
   };
+  useEffect(() => {
+    console.log(prediction);
+    if (prediction) {
+      navigate("/results", { state: { prediction: prediction } });
+    }
+  }, [prediction]);
   const [currentPage, setCurrentPage] = useState(1);
   const onContinue = () => {
     setCurrentPage(currentPage + 1);
