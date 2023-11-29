ba33c4e4e09343ecc94a8d2d48deedaf7e5e3ec1
TheHuntsman4 <anikethvij464@gmail.com>
2023-11-30 00:51:42 +0530

    <feat>: [integrates fetchData function to swelling]

--- klepio-frontend/src/pages/swellingPages/swelling1.jsx
+++ klepio-frontend/src/pages/swellingPages/swelling1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { QuestionPage2 } from "../../components";
 import { useMediaQuery } from "react-responsive";
 import DeskTopGreenBG from "../../assets/nonicons/DesktopFullGreenBG.png";
@@ -17,36 +17,42 @@ const Swelling1 = () => {
   const [pain, setPain] = useState("");
   const [changes, setChanges] = useState("");
   const [normal, setNormal] = useState("");
+  const [prediction, setPrediction] = useState();
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-device-width: 1024px)",
   });
 
-  const onContinueEnd = () => {
+  const onContinueEnd = async () => {
     answers = {
       ...answers,
-      "Nature_of_Pain": "0",
-      "Severity_of_pain": "0",
-      "Onset_and_mode_of_pain": "0",
-      "Factors_which_worsens_the_pain": "0",
-      "Is_the_swelling_painful": pain,
-      "Has_the_swelling_changed_since_it_was_first_noticed": changes,
-      "Does_the_swelling_changes_during_normal_activities": normal,
-      "Is_the_ulcer_painful": "0",
-      "Is_there_bleeding_from_the_ulcer": "0",
-      "Is_there_discharge_from_the_ulcer": "0",
-      "Is_there_a_foul_smell_from_the_ulcer": "0",
-      "Do_the_ulcers_interfere_with_daily_activities": "0",
-      "Has_the_ulcer_changed_since_first_noticed": "0",
-      "Have_you_had_similar_ulcers": "0",
-      "Is_there_bleeding_in_the_gums": "0",
-      "Is_there_pain_in_the_gums": "0",
-      "If_any_tooth_teeth_is_are_mobile_what_is_the_degree_of_mobility": "0",
+      Nature_of_Pain: "0",
+      Severity_of_pain: "0",
+      Onset_and_mode_of_pain: "0",
+      Factors_which_worsens_the_pain: "0",
+      Is_the_swelling_painful: pain,
+      Has_the_swelling_changed_since_it_was_first_noticed: changes,
+      Does_the_swelling_changes_during_normal_activities: normal,
+      Is_the_ulcer_painful: "0",
+      Is_there_bleeding_from_the_ulcer: "0",
+      Is_there_discharge_from_the_ulcer: "0",
+      Is_there_a_foul_smell_from_the_ulcer: "0",
+      Do_the_ulcers_interfere_with_daily_activities: "0",
+      Has_the_ulcer_changed_since_first_noticed: "0",
+      Have_you_had_similar_ulcers: "0",
+      Is_there_bleeding_in_the_gums: "0",
+      Is_there_pain_in_the_gums: "0",
+      If_any_tooth_teeth_is_are_mobile_what_is_the_degree_of_mobility: "0",
     };
     console.log(answers);
-    // navigate("/diagnose/common", { state: { answers: answers } });
-    fetchData(answers);
+    const response = await fetchData(answers);
+    setPrediction(response);
   };
-
+  useEffect(() => {
+    console.log(prediction);
+    if (prediction) {
+      navigate("/results", { state: { prediction: prediction } });
+    }
+  }, [prediction]);
   const [currentPage, setCurrentPage] = useState(1);
   const onContinue = () => {
     setCurrentPage(currentPage + 1);
